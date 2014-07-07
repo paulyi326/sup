23a916eea1a4753102948c32289ee41346ed7338
Paul Yi <paul.yi326@gmail.com>
2014-07-07 16:01:18 -0700

    added references to firebase, angularfire, and firebase simple login into index.html

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('sup', ['ionic', 'sup.controllers', 'sup.services'])
+angular.module('sup', ['ionic', 'sup.controllers', 'sup.services', 'firebase'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
