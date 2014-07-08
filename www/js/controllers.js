b4e944eb3c385d17154da4039ae688d80616398b
Paul Yi <paul.yi326@gmail.com>
2014-07-08 10:34:15 -0700

    firing event on child remove when clicking on a friends name

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,8 +6,9 @@ angular.module('sup.controllers', [])
   $scope.createUser = function() {
     // console.log('user email: ', $scope.user.email);
     // console.log('user password: ', $scope.user.password);
-    console.log($scope.user);
+    // console.log($scope.user);
     User.create($scope.user);
+    $location.path('/tab/friends');
   };
 
   $scope.loginUser = function() {
@@ -30,15 +31,26 @@ angular.module('sup.controllers', [])
 
 .controller('FriendsCtrl', function($scope, User) {
   $scope.friends = User.all;
-  console.log($scope.friends);
+  // console.log($scope.friends);
 
   $scope.addFriend = function(friend) {
     User.create(friend);
   };
 
-  $scope.sup = function() {
-    console.log('SUP');
+  $scope.sup = function(friendId) {
+    console.log(friendId);
+    /* 
+      I can log friendId here, meaning when I click, I can add data
+      to the friend on firebase. So I can add a sup property and
+      on that change, trigger some event from the friend's persepective
+    */
+    User.sup(friendId);
   }
+
+  // $scope.sup = function() {
+  //   console.log('SUP');
+  //   Sup.sup();
+  // }
 })
 
 .controller('FriendDetailCtrl', function($scope, $stateParams, User) {
--- www/js/services.js
+++ www/js/services.js
@@ -4,10 +4,28 @@ angular.module('sup.services', [])
  * A simple example service that returns some data.
  */
 
+// .factory('Sup', function($firebase, FIREBASE_URL) {
+//   var ref = new Firebase(FIREBASE_URL + 'users/*/');
+//   ref.on('child_added', function(childSnapshot) {
+//     alert(childSnapshot.val());
+//   });
+
+//   var sups = $firebase(ref);
+
+//   var Sup = {
+//     sup: function() {
+//       return sups.$add('sup');
+//     }
+//   };
+
+//   return Sup;
+// })
+
 .factory('User', function($firebase, FIREBASE_URL) {
   // Might use a resource here that returns a JSON array
 
   var ref = new Firebase(FIREBASE_URL + 'users');
+
   var users = $firebase(ref);
 
   var User = {
@@ -20,6 +38,15 @@ angular.module('sup.services', [])
     },
     delete: function(userId) {
       return users.$remove(userId);
+    },
+    sup: function(id) {
+      var userRef = ref.child(id);
+      userRef.on('child_removed', function() {
+        console.log('child was removed');
+      })
+      userRef.update({sup: true});
+      var supRef = userRef.child('sup');
+      supRef.set(null);
     }
   };
 
